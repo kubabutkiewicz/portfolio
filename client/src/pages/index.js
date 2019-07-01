783854938c66796ee7b721f467f982ab1ceb9f35
Kuba Butkiewicz <jakub.butkiewicz94@gmail.com>
2019-07-01 14:41:17 +0200

    Change name of folder front-end to client

---
+++ client/src/pages/index.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+import Layout from "../components/layout"
+import Image from "../components/image"
+import SEO from "../components/seo"
+
+const IndexPage = () => (
+  <Layout>
+    <SEO title="Home" />
+    <h1>hello world</h1>
+  </Layout>
+)
+
+export default IndexPage
